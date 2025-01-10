0e43d5cfd9a6ce7a41083e38bc0329fa127c9e0f
Dreamblader <carlosgabrielmaster@gmail.com>
2025-01-10 19:42:41 -0300

    can check unsecured ssl shortners

--- app/services.py
+++ app/services.py
@@ -1,5 +1,6 @@
 import urllib.request
 from .models import URL
+import ssl
 
 def getURL(url):
     try:
@@ -12,12 +13,14 @@ def getURL(url):
 
 
 def __getURLInRedirect(url):
+    context = ssl._create_unverified_context()
     req = urllib.request.Request(
     url=url, 
     headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)'}
     )
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen(req, context=context) as response:
         full_url = response.geturl()
+        print(response)
         if full_url != url:
             model = URL(final_url = full_url, short_url=url, checks=1)
             model.save()
