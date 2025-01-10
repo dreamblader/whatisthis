1ccedd54f3c135b113bde14f94e15ea8c9319e55
Dreamblader <carlosgabrielmaster@gmail.com>
2025-01-10 14:04:29 -0300

    adding stuff for deploy

--- whatisthis/settings.py
+++ whatisthis/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l2+^77zdjyh6ls66=+a#l7e4^#-01y$j@494kxkl8kpdo8ir%!'
+SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-l2+^77zdjyh6ls66=+a#l7e4^#-01y$j@494kxkl8kpdo8ir%!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
