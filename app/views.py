2a2ec5edba92445e005314dfcfeecfa32e880e31
Dreamblader <carlosgabrielmaster@gmail.com>
2025-01-10 17:29:53 -0300

    error print in debug

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
 from .forms import ShortURLForm
+from django.conf import settings
 from . import services
 
 
@@ -11,7 +12,9 @@ def home(request):
     if form.is_valid():
         try:
             result = services.getURL(form.cleaned_data["short_url"])
-        except:
+        except Exception as error:
+            if settings.DEBUG:
+                print(error)
             error = True
     
     context = {"form": form, "result":result, "error":error}
