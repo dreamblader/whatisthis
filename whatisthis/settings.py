583ae5c56067401b64ada4b08044b2f35d0764df
Dreamblader <carlosgabrielmaster@gmail.com>
2025-01-10 17:08:52 -0300

    added gunicorn

--- whatisthis/settings.py
+++ whatisthis/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-l2+^77zdjyh6ls66=+a#l7e4^#-01y$j@494kxkl8kpdo8ir%!')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", '') != False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
